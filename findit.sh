bdc31ae71d918e3865ed053f89fc34e47bee4c17
Kolen Cheung <christian.kolen@gmail.com>
2017-02-18 13:54:20 -0800

    findit.sh: rename FOLDER to dir (minor)

--- findit.sh
+++ findit.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
-# usage: ./findit.sh FOLDER
+# usage: ./findit.sh <directory>
 # expects a sidecar of .cat file besides every .fits file. If the .cat file does not exist, return the corresponding .fits filenames in the stdout
 # Notes:
 # /usr/bin/env bash is used to use the user's choice of bash rather than default bash
 # parameters:
 # *depth to control how deep you want to search into
 
-FOLDER="$1"
+dir="$1"
 
-if [[ ! -d "$FOLDER" ]]; then
-  printf "%s\n" "$FOLDER is not a directory or does not exist."
+if [[ ! -d "$dir" ]]; then
+  printf "%s\n" "$dir is not a directory or does not exist."
   exit 1
 fi
 
-find "$FOLDER" -maxdepth 1 -mindepth 1 -name "*.fits" -exec bash -c '
+find "$dir" -maxdepth 1 -mindepth 1 -name "*.fits" -exec bash -c '
   for fitsFile do
     expectedFile="${fitsFile%.*}.cat"
     if [[ ! -e "$expectedFile" ]]; then
