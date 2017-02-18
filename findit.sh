682219d0b11801fc5ad6b9a11f07532122bfc32b
Kolen Cheung <christian.kolen@gmail.com>
2017-02-18 13:53:14 -0800

    findit.sh: catch the exception when the FOLDER is not a directory

--- findit.sh
+++ findit.sh
@@ -7,7 +7,14 @@
 # parameters:
 # *depth to control how deep you want to search into
 
-find "$1" -maxdepth 1 -mindepth 1 -name "*.fits" -exec bash -c '
+FOLDER="$1"
+
+if [[ ! -d "$FOLDER" ]]; then
+  printf "%s\n" "$FOLDER is not a directory or does not exist."
+  exit 1
+fi
+
+find "$FOLDER" -maxdepth 1 -mindepth 1 -name "*.fits" -exec bash -c '
   for fitsFile do
     expectedFile="${fitsFile%.*}.cat"
     if [[ ! -e "$expectedFile" ]]; then
