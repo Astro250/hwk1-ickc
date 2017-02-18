8b7cc4575b5407d2461a005f2f395a20419dc74e
Kolen Cheung <christian.kolen@gmail.com>
2017-02-18 13:41:34 -0800

    findit.sh: put .fits instead of .cat in the stdout according to new requirement

--- findit.sh
+++ findit.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # usage: ./findit.sh FOLDER
-# expects a sidecar of .cat file besides every .fits file. If the .cat file does not exist, return the expected filenames in the stdout
+# expects a sidecar of .cat file besides every .fits file. If the .cat file does not exist, return the corresponding .fits filenames in the stdout
 # Notes:
 # /usr/bin/env bash is used to use the user's choice of bash rather than default bash
 # parameters:
@@ -11,6 +11,6 @@ find "$1" -maxdepth 1 -mindepth 1 -name "*.fits" -exec bash -c '
   for f do
     expectedFile="${f%.*}.cat"
     if [[ ! -e "$expectedFile" ]]; then
-      printf "%s\n" "$expectedFile"
+      printf "%s\n" "$f"
     fi
   done' bash {} +
