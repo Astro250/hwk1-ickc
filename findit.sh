de93c6028d6453af02fa7d18bca9a4f1bad89aa2
Kolen Cheung <christian.kolen@gmail.com>
2017-02-18 13:57:25 -0800

    findit.sh: add comments (minor)

--- findit.sh
+++ findit.sh
@@ -9,11 +9,13 @@
 
 dir="$1"
 
+# check dir is indeed a directory
 if [[ ! -d "$dir" ]]; then
   printf "%s\n" "$dir is not a directory or does not exist."
   exit 1
 fi
 
+# put .fits to stdout if corresponding .cats is not found
 find "$dir" -maxdepth 1 -mindepth 1 -name "*.fits" -exec bash -c '
   for fitsFile do
     expectedFile="${fitsFile%.*}.cat"
